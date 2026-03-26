791d03a57dd1904b147fb265eecf4b76934b1f41
Jhon Aguilera <jfaguilera833@gmail.com>
2026-03-26 02:03:05 +0000

    cambios creados del proyecto

--- MiPrimerAngular/src/app/app.routes.ts
+++ MiPrimerAngular/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
+import {} from './pages/inicio/inicio'; /*funcion para llamar la carpeta en el archivo general*/
+import {} from './pages/contactos/contactos'; /*funcion para llamar la carpeta en el archivo general*/
+import {} from './pages/ejercicios/ejercicios'; /*funcion para llamar la carpeta en el archivo general*/
 export const routes: Routes = [];
